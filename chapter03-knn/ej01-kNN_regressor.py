c24fd2803d4e975139da3837973137466b85891e
Alejandro H. Pineda <ale.hpineda@gmail.com>
2019-05-07 19:35:07 -0500

    Finished ch03 kNN

--- chapter03-knn/ej01-kNN_regressor.py
+++ chapter03-knn/ej01-kNN_regressor.py
@@ -50,4 +50,102 @@ class Regression(object):
             return value
 
 
+class RegresionTest(object):
+    """
+    Lee los datos, calcula y grafica el error
+    de la regresion kNN 
+    """
+
+    def __init__(self):
+        self.houses = None
+        self.values = None
+
+    
+    def load_csv_file(self, csv_file, limit = None):
+        """
+        Carga el archivo CSV con datos de las casas.
+        :param csv_file: Nombre del archivo CSV
+        :param limit: Limite del numero de filas
+        """
+
+        houses = pd.read_csv(csv_file, nrows=limit)
+        self.values = houses['AppraisedValue']
+        houses = houses.drop('AppraisedValue', 1)
+        houses = (houses - houses.mean()) / (houses.max() - houses.min())
+        self.houses = houses
+        self.houses = self.houses[['lat', 'long', 'SqFtLot']]
+
+
+    def tests(self, folds):
+        """
+        Calcular los errores promedio absolutos para los test
+        :param folds: Numero de veces que los datos se repartieron
+        :return: lista de los valores de los errores
+        """
+
+        holdout = 1 / float(folds)
+        errors = []
+        for _ in range(folds):
+            values_regress, values_actual = self.test_regression(holdout)
+            errors.append(mean_absolute_error(values_actual, values_regress))
+
+        return errors
+
+    
+    def test_regression(self, holdout):
+        """
+        Calcula la regresion para datos fuera de la muestra
+        :param holdout: Parte de los datos a probar [0,1]
+        :return: tuple(y_regression, values_actual)
+        """
+
+        test_rows = random.sample(self.houses.index.tolist(), int(round(len(self.houses) * holdout)))
+        train_rows = set(range(len(self.houses))) - set(test_rows)
+        df_test = self.houses.ix[test_rows]
+        df_train = self.houses.drop(test_rows)
+
+        train_values = self.values.ix[train_rows]
+        regression = Regression()
+        regression.set_data(houses=df_train, values=train_values)
+
+        values_regr = []
+        values_actual = []
+
+        for idx, row in df_test.iterrows():
+            values_regr.append(regression.regress(row))
+            values_actual.append(self.values[idx])
+
+        return values_regr, values_actual
+
+    def plot_error_rates(self):
+        """
+        Plot MAE vs #folds
+        """
+        folds_range = range(2,11)
+        errors_df = pd.DataFrame({'max':0, 'min':0}, index=folds_range)
+        
+        for folds in folds_range:
+            errors = self.tests(folds)
+            errors_df['max'][folds] = max(errors)
+            errors_df['min'][folds] = min(errors)
+        
+        errors_df.plot(title='Error promedio absoluto de kNN con diferente rango de folds')
+        plt.xlabel('#folds_range')
+        plt.ylabel('MAE')
+        plt.show()
+
+
+import os
+
+here = os.path.dirname(os.path.abspath(__file__))
+filename = os.path.join(here, 'king_county_data_geocoded.csv')
+
+
+def main():
+    regression_test = RegresionTest()
+    regression_test.load_csv_file(filename)
+    regression_test.plot_error_rates()
+
+if __name__ == "__main__":
+    main()
 
